9fe723cb5352b15fd9f02f3f31d17bbdd91637e7
Interiority <interiority@iotech.ie>
2020-06-04 19:25:27 +0100

    Serial MUX GUI working

--- SerialMux2.py
+++ SerialMux2.py
@@ -3,6 +3,7 @@ Main routine for Serial Mux
 Forked from the Comm module of SD-Node
 Takes two serial inputs and logs them.
 Optionally, forward to a UDP address:port
+Tested with Python >=3.6
 
 By: JOR
     v0.1    26APR20     First go!
@@ -20,7 +21,7 @@ output_file = open(output_filename, 'a', newline='')
 # Open a UDP server, even through we will not receive
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Hard code the destination port
+# Hard code the destination port, expect KPlex, but can be anything
 kplex_IPv4 = "127.0.0.1"
 kplex_Port = 2001
 print("Expecting to find UDP receiver at " + kplex_IPv4 + ":" + str(kplex_Port))
@@ -56,12 +57,14 @@ Serial_Port2.flushInput()
 try:
     print("press [ctrl][c] at any time to exit...")
     while 1:
-        # Receive data from the serial link and relay to UDP server
+        # Receive data from serial link 1
         read_buffer1 = Serial_Port1.readline().decode('ascii', errors='replace')
         while len(read_buffer1) != 0:
             try:
                 current_line = str(read_buffer1)
+                # Send to UDP server
                 sock.sendto(bytes(current_line, "utf-8"), (kplex_IPv4, kplex_Port))
+                # Log the data
                 output_file.writelines(current_line)
                 print('GPS:' + current_line.strip())
             except Exception as error:
@@ -69,12 +72,14 @@ try:
             finally:
                 read_buffer1 = Serial_Port1.readline().decode('ascii', errors='replace')
 
-        # Receive data from the serial link and relay to UDP server
+        # Receive data from serial link 2
         read_buffer2 = Serial_Port2.readline().decode('ascii', errors='replace')
         while len(read_buffer2) != 0:
             try:
                 current_line = str(read_buffer2)
+                # Send to UDP server
                 sock.sendto(bytes(current_line, "utf-8"), (kplex_IPv4, kplex_Port))
+                # Log the data
                 output_file.writelines(current_line)
                 print('Magnetometer:' + current_line.strip())
             except Exception as error:
